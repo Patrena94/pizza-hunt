d4bf9ae2f947debea7e2e045fd9bce4fb78fde0c
Vashti <patrena@sbcglobal.net>
2021-09-06 22:19:27 -0700

    start of comment

--- models/Pizza.js
+++ models/Pizza.js
@@ -9,15 +9,37 @@ const PizzaSchema = new Schema({
   },
   createdAt: {
     type: Date,
-    default: Date.now
+    default: Date.now  
   },
   size: {
     type: String,
     default: 'Large'
+}, 
+toppings: [],
+
+comments: [
+  { 
+    type: Schema.Types.ObjectId,
+    ref:'Comment'
+  
+  }
+]
+},
+{
+  toJSON:{
+    virtual:true,
   },
-  toppings: []
-});
+  id:false
+}
+);
+
+//get total count of comments and replies on retrieval
 
 const Pizza = model('Pizza', PizzaSchema);
+
+PizzaSchema.virtual('commentCount').get(function() {
+  return this.comments.length;
+
+});
 
 module.exports = Pizza;
